d21aa5f5f87c2c4da26b8c833029870f6515e07e
rzavyalov <electrotehnik@gmail.com>
2025-10-30 14:44:10 +0500

    Задачи выполнены

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         //Задача1. Демонстрация метода, которые меняет местами два элемента массива.
@@ -5,17 +7,36 @@ public class Main {
         String[]    strings = {"Каждый", "Охотник", "Желает", "Знать"};
         Integer[]   numbers = {0, 1, 2, 3};
 
-        //Благодаря универсальному параметру, используем один и тот же метод для разных типов данных
+        /*  Выполняем задачу 2.
+            Преобразуем массив в ArrayList, с любыми ссылочными типами данных на входе
+         */
+        Mapper mapper = new Mapper();
+        ArrayList<String> arrListStrings = mapper.toMap(strings);
+        ArrayList<Integer> arrListNumbers = mapper.toMap(numbers);
+
+        /*  Благодаря универсальному параметру,
+            используем один и тот же метод для разных типов данных
+         */
         Swapper swapper = new Swapper();
         swapper.swap(strings, 0, 3);
         swapper.swap(numbers, 0, 3);
 
-        //Выводим результаты
+        //Выводим результаты по задаче 1
         for(String item : strings) {
             System.out.print(item + ", ");
         }
         System.out.println(" ");
         for(Integer item : numbers) {
+            System.out.print(item + ", ");
+        }
+
+        //Выводим результаты по задаче 2
+        System.out.println(" ");
+        for(String item : arrListStrings) {
+            System.out.print(item + ", ");
+        }
+        System.out.println(" ");
+        for(Integer item : arrListNumbers) {
             System.out.print(item + ", ");
         }
     }
--- src/Mapper.java
+++ src/Mapper.java
@@ -3,7 +3,6 @@ import java.util.Arrays;
 
 public class Mapper {
     public <T> ArrayList<T> toMap(T[] array) {
-        ArrayList<T> arrayList = new Arrays.asList(array);
-        return arrayList;
+        return new ArrayList<T>(Arrays.asList(array));
     }
 }
